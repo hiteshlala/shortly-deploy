dbb2a165b102b927852dddcdd8661411569bf93b
Hack Reactor Students <accounts@hackreactor.com>
2016-02-11 15:40:39 -0800

    Updating server...

--- Gruntfile.js
+++ Gruntfile.js
@@ -86,7 +86,7 @@ module.exports = function(grunt) {
     },
   });
 
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-uglify')
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
